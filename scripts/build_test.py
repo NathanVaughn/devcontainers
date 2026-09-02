22c8fcf6dfd0de84fedfc13eeee635d4b9f5def6
Nathan Vaughn <nath@nvaughn.email>
2026-09-01 20:12:41 -0500

    Update how uv works

---
+++ scripts/build_test.py
@@ -0,0 +1,35 @@
+import argparse
+import pathlib
+import subprocess
+import sys
+
+ROOT_DIR = pathlib.Path(__file__).parent.parent
+
+
+def build(image: str) -> None:
+    sys.exit(
+        subprocess.run(
+            [
+                "docker",
+                "build",
+                "-t",
+                f"devcontainer:{image}",
+                "-f",
+                f"{image}/Dockerfile",
+                ".",
+            ],
+            cwd=ROOT_DIR,
+            stderr=sys.stderr,
+            stdout=sys.stdout,
+            stdin=sys.stdin,
+            check=False,
+        ).returncode
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", choices=["python", "node", "pythonnode"])
+    args = parser.parse_args()
+
+    build(args.image)
